8b55b8cca79db22f1a40953ea511542b30db9031
hadeelbkh <hadeel.b14@gmail.com>
2024-08-11 23:27:11 +0300

    adding files

--- cypress/integration/examples/progifts.js
+++ cypress/integration/examples/progifts.js
@@ -3,18 +3,30 @@
 
 import 'cypress-iframe';
 
-describe('Progifts Website Test', () => {
-    it('Send Nike card to multiple recipients', () => {
+describe('Progifts Website Test', ()=> {
+    before(() => {
+        cy.fixture('example').as('recipientData');
+    });
+
+    it('Send Nike card to multiple recipients', ()=> {
 
         cy.visit('https://giftit-ng-ui-qa.progifts.io/catalog');
 
-        //Choose Nike card
-        cy.get('.catalog-search').type('nike');
+        //Look for 'golfsmith' card, if not found, choose 'Nike'
+        cy.get('.catalog-search').type('golfsmith');
         cy.get('button').contains('Search').click();
-        cy.get('app-store-item').contains('Nike').click();
-
+        cy.get('.container').contains("We couldn’t find anything for").then((cards) => {
+            if(cards.length){
+                cy.get('.catalog-search').clear().type('nike');
+                cy.get('button').contains('Search').click();
+                cy.get('app-store-item').contains('Nike').click();
+            } else {
+                cy.log("Found. Do Nothing.")
+            }
+        });
+        
         cy.url().should('include', '/products');
-
+        
         //Choose amount
         cy.get('app-nomination').contains('$75').click();
         cy.get('button.continue').contains('Continue').click();
@@ -25,16 +37,20 @@ describe('Progifts Website Test', () => {
         cy.get('.form-switch button').contains('Multiple recipients').click();
         cy.get('button.add-giftee').contains('Add recipient').click();
 
-        cy.get('input[name="recipient-name"]').eq(0).type('Qasem', { force: true });
-        cy.get('input[name="recipient-email"]').eq(0).type('qdaqqa@itgsoftware.com', { force: true });
+        cy.get('@recipientData').then((data) => {
+            cy.addRecipients(data.name, data.email); // addRecipients command: implemented in the commands.js file
+        });
 
-        cy.get('button.add-recipient').contains('Add recipients').click();
 
-        cy.get('input[name="recipient-name"]').eq(1).type('User', { force: true });
-        cy.get('input[name="recipient-email"]').eq(1).type('user@test.com', { force: true });
+        cy.removeRecipients("qdaqqa@itgsoftware.com"); // removeRecipients command: implemented in the commands.js file
+        cy.removeRecipients("user1@test.com");
+        cy.wait(5000);
 
         cy.get('#form-submit-btn').contains("Apply").click({ force: true });
         cy.get('#form-submit-btn').contains("Continue").click();
+
+
+
 
         cy.url().should('include', '/who-is-it-from');
 
@@ -47,6 +63,18 @@ describe('Progifts Website Test', () => {
         cy.url().should('include', '/checkout');
 
         //Perform payment
+        /*cy.iframe('iframe[name="__privateStripeFrame6993"]').find('input').type('4111111111111111');
+        cy.wait(1000);
+        cy.get('#name').type('Qasem');
+        cy.wait(1000);
+        cy.iframe('iframe[name="__privateStripeFrame6994"]').find('input').type('0326');
+        cy.wait(1000);
+        cy.iframe('iframe[name="__privateStripeFrame6995"]').find('input').type('111');
+        cy.wait(1000);
+        cy.get('#form-submit-btn').contains("Checkout").click();    
+        cy.wait(15000);
+        cy.url().should('include', '/success');
+        */
         cy.get('ngx-stripe-card-number iframe').its('0.contentDocument.body').find('input[name="cardnumber"]').type('4111111111111111');
         cy.wait(1000);
         cy.get('#name').type('Qasem');
@@ -56,7 +84,9 @@ describe('Progifts Website Test', () => {
         cy.get('ngx-stripe-card-cvc iframe').its('0.contentDocument.body').find('input[name="cvc"]').type('111');
         cy.wait(1000);
         cy.get('#form-submit-btn').contains("Checkout").click();    
-        cy.wait(10000);
-        cy.url().should('include', '/success');
+        cy.wait(5000);
+        cy.get('.message h2').should('contain', 'Your gift is on its way!');
+        
+
     });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,27 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+import 'cypress-iframe';
+
+Cypress.Commands.add("addRecipients", (rname, remail)=>{
+    rname.forEach((name, i) => {
+        cy.get('input[name="recipient-name"]').eq(i).type(name, { force: true });
+        cy.get('input[name="recipient-email"]').eq(i).type(remail[i], { force: true });
+        if (i < rname.length-1){
+        cy.get('button.add-recipient').contains('Add recipients').click();
+        }
+    });
+});
+
+Cypress.Commands.add("removeRecipients", (remail) => {
+    cy.get('tbody[formarrayname="receivers"] tr').each(($row) => {
+        cy.wrap($row).find('td').eq(2).find('input[name="recipient-email"]').then(($emailInput) => {
+            const emailText = $emailInput.val().trim();
+            if (emailText === remail) {
+                cy.wrap($row).find('img.remove-row').click({ force: true });
+                return false;
+            }
+        });
+    });
+});
+
